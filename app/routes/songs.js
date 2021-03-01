a0c7905abdb2cc1ce6909e40ee187eb6da1cc8ca
Lisa Au <lisa.au@crowdstrike.com>
2021-02-28 23:54:40 -0800

    Move songs model from application to route

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,4 +1,32 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+let Song = EmberObject.extend({
+    title:'',
+    band:'',
+    rating: 0
+});
 
 export default class SongsRoute extends Route {
+    model() {
+        let blackDog = Song.create({
+            title: 'Black Dog',
+            band: 'Led Zepplin',
+            rating: 3
+        });
+
+        let yellowLedbetter = Song.create({
+            title:'Yellow Ledbetter',
+            band:'Pearl Jam',
+            rating: 4
+        });
+
+        let pretender = Song.create({
+            title:'The Pretender',
+            band:'Foo Fighters',
+            rating: 2
+        });
+            
+        return [blackDog, yellowLedbetter, pretender];
+    }
 }
