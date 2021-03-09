9f6c60c376425cfda3bf78c024de76b995197238
Lisa Au <lisa.au@crowdstrike.com>
2021-03-08 22:44:32 -0800

    Fix route nesting for dynamic segment

--- app/router.js
+++ app/router.js
@@ -8,10 +8,8 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('bands', function() {
-    this.route('songs');
-
-    this.route('band', {
-      path: ':slug'
+    this.route('band', { path: 'slug' }, function() {
+      this.route('songs');
     });
-  })
+  });
 });
