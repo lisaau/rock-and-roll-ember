8094632d343560e969ac7953193125ecad6f0666
Lisa Au <lisa.au@crowdstrike.com>
2021-03-08 22:53:05 -0800

    Update data and class definition and add album route

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -6,11 +6,56 @@ const Band = EmberObject.extend({
     name:'',
 });
 
+const Song = EmberObject.extend({
+  title: '',
+  rating: 0,
+  band:''
+});
+
 export default class BandsRoute extends Route {
     model() {
-        let ledZeppelin = Band.create({ name:'Led Zeppelin' });
-        let pearlJam = Band.create({ name:'Pearl Jam' });
-        let fooFighters = Band.create({ name:'Foo Fighters' });
-        return A([ledZeppelin, pearlJam, fooFighters]);
+      let blackDog = Song.create({
+        title: 'Black Dog',
+        band: 'Led Zeppelin',
+        rating: 3
+      });
+
+      let yellowLedbetter = Song.create({
+        title: 'Yellow Ledbetter',
+        band: 'Pearl Jam',
+        rating: 4
+      });
+
+      let pretender = Song.create({
+        title: 'The Pretender',
+        band: 'Foo Fighters',
+        rating: 2
+      });
+      
+      let daughter = Song.create({
+        title:'Daughter',
+        band: 'Pearl Jam',
+        rating:5
+      });
+
+      let ledZeppelin = Band.create({
+        name: 'Led Zeppelin',
+        slug: 'led-zeppelin',
+        songs: A([blackDog])
+      });
+
+      let pearlJam = Band.create({
+        name: 'Pearl Jam',
+        slug: 'pearl-jam',
+        songs: A([yellowLedbetter, daughter])
+      });
+      
+      let fooFighters = Band.create({
+        name: 'Foo Fighters',
+        slug: 'foo-fighters',
+        songs: A([pretender])
+      });
+
+      return A([ledZeppelin, pearlJam, fooFighters]);
     }
 }
