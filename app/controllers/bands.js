6530c29612f36f7706f9c8b8f1ced62135be35a9
Lisa Au <lisa.au@crowdstrike.com>
2021-03-09 15:25:31 -0800

    Disable add button when empty

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,11 +1,14 @@
 import Controller from '@ember/controller';
 import { action } from'@ember/object';
 import Band from 'rarwe/models/band';
+import { empty } from'@ember/object/computed';
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName:'',
 
+  isAddButtonDisabled: empty('newBandName'),
+  
   addBand: action (function() {
     this.set('isAddingBand', true);
   }),
