926bfeebac3da6f9e6e403e7dbb2d537c751e7b7
Lisa Au <lisa.au@crowdstrike.com>
2021-03-13 16:42:05 -0800

    Add description for details template

--- app/routes/bands/band.js
+++ app/routes/bands/band.js
@@ -1,8 +1,19 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  router: service(),
+
   model(params) {
     let bands = this.modelFor('bands');
     return bands.find(band => band.slug === params.slug); //params.slug is now 'pearl-jam'
+  },
+
+  redirect(band) {
+    if (band.description) {
+      this.router.transitionTo('bands.band.details');
+    } else {
+      this.router.transitionTo('bands.band.songs');
+    }
   }
 });
